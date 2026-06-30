b8b72aadf185fca53da66c6f2d5912440efb9803
DevCodeYuvraj <yuvrajsingh27052002@gmail.com>
2026-06-30 10:43:33 +0000

    A2234:13 PM181 C18113i4:13 PM

---
+++ nextrun-clean-and-elegant-admin-dashboard/src/data/stats.js
@@ -0,0 +1,33 @@
+import {
+  MdGroup,
+  MdNotifications,
+  MdVisibility,
+  MdThumbUp,
+} from "react-icons/md";
+
+export const stats = [
+  {
+    title: "Followers",
+    value: "932",
+    icon: MdGroup,
+    color: "#4F46E5",
+  },
+  {
+    title: "Subscribers",
+    value: "1,032",
+    icon: MdNotifications,
+    color: "#00C2FF",
+  },
+  {
+    title: "Total Views",
+    value: "102K",
+    icon: MdVisibility,
+    color: "#7C3AED",
+  },
+  {
+    title: "Total Likes",
+    value: "32K",
+    icon: MdThumbUp,
+    color: "#EC4899",
+  },
+];
