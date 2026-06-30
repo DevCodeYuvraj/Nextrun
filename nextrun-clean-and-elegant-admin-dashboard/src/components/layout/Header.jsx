0e1475cad27f7eae5f74e0a738de8d8339882464
DevCodeYuvraj <yuvrajsingh27052002@gmail.com>
2026-06-30 10:33:19 +0000

    A8884:03 PM181 C18103i4:03 PM

--- nextrun-clean-and-elegant-admin-dashboard/src/components/layout/Header.jsx
+++ nextrun-clean-and-elegant-admin-dashboard/src/components/layout/Header.jsx
@@ -1,7 +1,38 @@
+import styles from "./Header.module.css";
+
+import {
+  MdNotificationsNone,
+  MdSettings
+} from "react-icons/md";
+
 export default function Header() {
   return (
-    <header className="header">
-      Header
+    <header className={styles.header}>
+
+      <div className={styles.left}>
+        <h2>Dashboard</h2>
+      </div>
+
+      <div className={styles.right}>
+
+        <input
+          className={styles.search}
+          type="text"
+          placeholder="Search here..."
+        />
+
+        <div className={styles.iconButton}>
+          <MdNotificationsNone size={22}/>
+        </div>
+
+        <div className={styles.iconButton}>
+          <MdSettings size={22}/>
+        </div>
+
+        <div className={styles.avatar}></div>
+
+      </div>
+
     </header>
   );
 }
