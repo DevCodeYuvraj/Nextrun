965f9e5808c0191eea34bd64c64a5fe40d1d5f56
DevCodeYuvraj <yuvrajsingh27052002@gmail.com>
2026-06-30 10:08:23 +0000

    A9363:38 PM181 C18138i3:38 PM

--- nextrun-clean-and-elegant-admin-dashboard/src/app/page.js
+++ nextrun-clean-and-elegant-admin-dashboard/src/app/page.js
@@ -1,3 +1,12 @@
+import MainLayout from "@/components/layout/MainLayout";
+
+export default function Dashboard() {
+  return (
+    <MainLayout>
+      Dashboard Content
+    </MainLayout>
+  );
+}
 export default function Home() {
   return (
     <h1>Nextrun Dashboard</h1>
