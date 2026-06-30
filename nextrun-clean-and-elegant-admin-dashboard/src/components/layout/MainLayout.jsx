b360be9e5fed6fbf6b1e2cb9ad1068077313c65a
DevCodeYuvraj <yuvrajsingh27052002@gmail.com>
2026-06-30 10:15:51 +0000

    A6773:45 PM181 C18145i3:45 PM

--- nextrun-clean-and-elegant-admin-dashboard/src/components/layout/MainLayout.jsx
+++ nextrun-clean-and-elegant-admin-dashboard/src/components/layout/MainLayout.jsx
@@ -1,3 +1,18 @@
-export default function MainLayout() {
-  return <div>MainLayout</div>;
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+
+export default function MainLayout({ children }) {
+  return (
+    <div className="layout">
+      <Sidebar />
+
+      <div className="main">
+        <Header />
+
+        <main className="content">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
 }
