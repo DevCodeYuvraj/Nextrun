9a96d975fb0b953955e4e01244031c8f5558fa31
DevCodeYuvraj <yuvrajsingh27052002@gmail.com>
2026-06-30 10:26:57 +0000

    A1343:56 PM181 C18156i3:56 PM

--- nextrun-clean-and-elegant-admin-dashboard/src/components/layout/Sidebar.jsx
+++ nextrun-clean-and-elegant-admin-dashboard/src/components/layout/Sidebar.jsx
@@ -1,7 +1,30 @@
+import styles from "./Sidebar.module.css";
+import { sidebarItems } from "@/data/sidebar";
+
 export default function Sidebar() {
   return (
-    <aside className="sidebar">
-      Sidebar
+    <aside className={styles.sidebar}>
+      <div className={styles.logo}>
+        Nex<span>trun.</span>
+      </div>
+
+      <nav className={styles.nav}>
+        {sidebarItems.map((item, index) => {
+          const Icon = item.icon;
+
+          return (
+            <div
+              key={item.title}
+              className={`${styles.item} ${
+                index === 0 ? styles.active : ""
+              }`}
+            >
+              <Icon size={20} />
+              <span>{item.title}</span>
+            </div>
+          );
+        })}
+      </nav>
     </aside>
   );
 }
